b9a2322208461c6d273399efefde343bac61044d
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2023-12-11 19:45:07 -0300

    exercício 4

--- Ex4.java
+++ Ex4.java
@@ -3,16 +3,32 @@ package matrizes;
 import java.util.Scanner;
 
 public class Ex4 {
-public static void main(String[] args) {}
-Scanner ler = new Scanner(System.in);
+public static void main(String[] args) {
+	Scanner ler = new Scanner(System.in);
 
-float notas [][] = new float[10][4];
+	float notas[][] = new float[10][4];
+	float medias[] = new float[10];
 
-for(int i=0; i<10; i++) {
-	System.out.println("Digite as 4 notas: ");
-	for(int j=0; j<4; j++) {
+	for(int i=0; i<10; i++) {
+		System.out.println("Digite as 4 notas: ");
+		for(int j=0; j<4; j++) {
+			notas[i][j] = ler.nextFloat();
+		}
+	}
+	for(int i=0; i<10; i++) {
+		float soma = 0;
+		for(int j=0; j<4; j++) {
+			soma += notas[i][j]; 
+		}
+		medias[i] = soma/4;
+	}
 	
-		notas[i][j] = ler.nextInt();
+	System.out.println("\nMédia dos participantes: ");
+	for(int i=0; i<10; i++) {
+	System.out.println(medias[i]);
+	}
+	ler.close();
 	}
 }
-}
+
+
